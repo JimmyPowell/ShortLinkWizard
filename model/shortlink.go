ace2fd8f654e1eec7390bac0aba9594dad4ee39d
JimmyPowell <null@mail>
2025-01-11 15:49:23 +0800

    添加了统计浏览量的逻辑

--- model/shortlink.go
+++ model/shortlink.go
@@ -11,6 +11,8 @@ type ShortLink struct {
 	OriginalURL string     `gorm:"type:text;not null"`
 	Code        string     `gorm:"type:varchar(10);unique_index;not null"`
 	ExpiresAt   *time.Time `gorm:"index;default:NULL"`
+	ViewCount   uint       `gorm:"default:0"`
+	IsDeleted   bool       `gorm:"default:false"`
 }
 
 func AutoMigrate(db *gorm.DB) error {
